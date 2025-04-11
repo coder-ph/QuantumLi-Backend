4059932beaaff665a6da12a63faac30be8efd67b
Phelix <phelixmbani@gmail.com>
2025-04-11 11:32:29 +0300

    sk revocation

--- Backend/src/config/config.py
+++ Backend/src/config/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Config:
   
    
-    SECRET_KEY = os.getenv('SECRET_KEY', '1234erdfch!!ghazswdxt')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
