1e0062653e9483998ea7a16796f8c808fd842ee6
Angela-G9 <angelagathoni9568@gmail.com>
2025-04-27 14:43:46 +0300

    addedblueprint

--- Backend/src/startup/routes.py
+++ Backend/src/startup/routes.py
@@ -7,7 +7,7 @@ from src.routers.drivers_router import drivers_bp
 from src.routers.incident_routes import incidents_bp
 from src.routers.client_routes import client_bp
 from src.routers.order_routes import orders_bp
-
+from src.routers.order_response_routes import order_responses_bp
 def register_routes(app):
      app.register_blueprint(auth_bp, url_prefix='/auth')
      app.register_blueprint(product_bp, url_prefix='/api/v1')
@@ -17,5 +17,6 @@ def register_routes(app):
      app.register_blueprint(incidents_bp, url_prefix='/api/v1')
      app.register_blueprint(client_bp, url_prefix='/api/v1')
      app.register_blueprint(orders_bp, url_prefix='/api/v1')
+     app.register_blueprint(order_responses_bp, url_prefix='/api/v1')
      
      
