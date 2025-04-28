54dc38845f08052aa6e852a9b5824480ca1ace3a
Edmund <edmundgachanja@gmail.com>
2025-04-28 09:35:30 +0300

    adds routes related to driver location

--- Backend/src/routers/drivers_router.py
+++ Backend/src/routers/drivers_router.py
@@ -4,7 +4,11 @@ from src.handlers.controllers.driver_controller import (
     get_all_drivers_view,
     get_driver_view,
     update_driver_view,
-    delete_driver_view
+    delete_driver_view,
+    create_driver_location_view,
+    get_all_driver_location_view,
+    get_driver_location_by_id_view,
+    update_driver_location_view
 )
 
 drivers_bp = Blueprint('drivers_bp', __name__)
@@ -14,3 +18,8 @@ drivers_bp.route('/drivers', methods=['GET'])(get_all_drivers_view)
 drivers_bp.route('/drivers/<uuid:driver_id>', methods=['GET'])(get_driver_view)
 drivers_bp.route('/drivers/<uuid:driver_id>', methods=['PUT'])(update_driver_view)
 drivers_bp.route('/drivers/<uuid:driver_id>', methods=['DELETE'])(delete_driver_view)
+
+drivers_bp.route('/drivers/location', methods=['POST'])(create_driver_location_view)
+drivers_bp.route('/drivers/location', methods=['GET'])(get_all_driver_location_view)
+drivers_bp.route('/drivers/location/<uuid:driver_id>', methods=['GET'])(get_driver_location_by_id_view)
+drivers_bp.route('/drivers/location/<uuid:driver_id>',methods=['PUT'])(update_driver_location_view)
