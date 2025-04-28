e59bc1428ec455b3171882cb2401ec66bf24a186
Edmund <edmundgachanja@gmail.com>
2025-04-28 14:06:31 +0300

    adds driver status api

--- Backend/src/routers/drivers_router.py
+++ Backend/src/routers/drivers_router.py
@@ -11,7 +11,8 @@ from src.handlers.controllers.driver_controller import (
     create_driver_location_view,
     get_all_driver_location_view,
     get_driver_location_by_id_view,
-    update_driver_location_view
+    update_driver_location_view,
+    update_driver_status_view
 )
 
 drivers_bp = Blueprint('drivers_bp', __name__)
@@ -27,7 +28,9 @@ drivers_bp.route('/drivers/<uuid:driver_id>/documents', methods=['POST'])(upload
 drivers_bp.route('/drivers/<uuid:driver_id>/documents', methods=['GET'])(get_documents_view)
 drivers_bp.route('/documents/<uuid:document_id>/status', methods=['PUT'])(update_document_status_view)
 
-drivers_bp.route('/drivers/location', methods=['POST'])(create_driver_location_view)
-drivers_bp.route('/drivers/location', methods=['GET'])(get_all_driver_location_view)
-drivers_bp.route('/drivers/location/<uuid:driver_id>', methods=['GET'])(get_driver_location_by_id_view)
-drivers_bp.route('/drivers/location/<uuid:driver_id>',methods=['PUT'])(update_driver_location_view)
+# drivers_bp.route('/drivers/location', methods=['POST'])(create_driver_location_view)
+# drivers_bp.route('/drivers/location', methods=['GET'])(get_all_driver_location_view)
+# drivers_bp.route('/drivers/location/<uuid:driver_id>', methods=['GET'])(get_driver_location_by_id_view)
+# drivers_bp.route('/drivers/location/<uuid:driver_id>',methods=['PUT'])(update_driver_location_view)
+
+drivers_bp.route('/drivers/status/<uuid:driver_id>',methods=['PUT'])(update_driver_status_view)
