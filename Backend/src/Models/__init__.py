a09dec5bd8164f99996ba42181bc0ce9e3c9248a
carole27888 <cmutemi27@gmail.com>
2025-05-06 11:39:02 +0300

    a lot of changes

--- Backend/src/Models/__init__.py
+++ Backend/src/Models/__init__.py
@@ -1,3 +1,31 @@
+from .audit_logs import Audit_Logs
+from .base_model import BaseModel
+from .billing import Billing
+from .carriers import Carrier
+from .client import Client
+from .customerfeedback import Customer_Feedback
+from .driverRating import Driver_Ratings
+# from .driverLocation import DriverLocation
+from .drivers import Driver
+from .driverSchedule import DriverSchedule
 from .employee import Employee
+from .enums import ShipmentStatusEnum, ShippingMethodEnum, DocumentType
+from .incidents import Incidents
+from .inventory import Inventory
+from .inventoryMovement import InventoryMovement
+from .invoiceitems import InvoiceItems
+from .locations import Location
+from .notification import Notifications
+from .orderItem import OrderItem
+from .orderResponse import OrderResponse
+from .orders import Order
+from .product import Product
+from .rates import Rates
+from .shipment import Shipment
+from .shipmentOrder import ShipmentOrder
+from .systemusers import System_Users
+from .thirdparty import ThirdPartyService
+from .trackevents import TrackingEvent
+from .vehicles import Vehicle
 from .warehouseOperations import WarehouseOperation
-# import all models here
+
