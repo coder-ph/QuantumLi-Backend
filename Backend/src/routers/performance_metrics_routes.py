8ece67f8b051858e50c45997b308868c2d7300eb
carole27888 <cmutemi27@gmail.com>
2025-05-07 12:30:02 +0300

    db.json

--- Backend/src/routers/performance_metrics_routes.py
+++ Backend/src/routers/performance_metrics_routes.py
@@ -9,7 +9,7 @@ from src.handlers.controllers.performance_metrics_controller import (
 performance_metrics_bp = Blueprint('performance_metrics_bp', __name__)
 
 
-performance_metrics_bp.route('/performance/deliveries-per-driver', methods=['GET'])(get_deliveries_per_driver)
-performance_metrics_bp.route('/performance/average-delivery-time', methods=['GET'])(get_average_delivery_time)
-performance_metrics_bp.route('/performance/order-acceptance-rejection', methods=['GET'])(get_order_acceptance_rejection)
-performance_metrics_bp.route('/performance/customer-ratings', methods=['GET'])(get_customer_ratings)
+performance_metrics_bp.route('/deliveries-per-driver', methods=['GET'])(get_deliveries_per_driver)
+performance_metrics_bp.route('/average-delivery-time', methods=['GET'])(get_average_delivery_time)
+performance_metrics_bp.route('/order-acceptance-rejection', methods=['GET'])(get_order_acceptance_rejection)
+performance_metrics_bp.route('/customer-ratings', methods=['GET'])(get_customer_ratings)
